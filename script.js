5e69214602bd975103c1b754ef4d07f4a7bc78a3
MireyMm <mizuizu77@gmail.com>
2025-10-06 20:51:26 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("sapaButton").addEventListener("click", function () {
+    alert("Hai, terima kasih sudah berkunjung ke M SHOP!");
+});
+
+function toggleDarkMode() {
+    document.body.classList.toggle("dark-mode");
+}
