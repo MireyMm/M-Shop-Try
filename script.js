a0060bf2ad349ab001545457b1b4fc1750cf5353
MireyMm <mizuizu77@gmail.com>
2025-10-06 20:52:03 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
+// Event ketika tombol "Sapa saya!" diklik
 document.getElementById("sapaButton").addEventListener("click", function () {
-    alert("Hai, terima kasih sudah berkunjung ke M SHOP!");
+    alert("Hai! Terima kasih sudah berkunjung ke M SHOP 😊");
 });
 
+// Toggle dark mode
 function toggleDarkMode() {
     document.body.classList.toggle("dark-mode");
-}
+
+    // Simpan preferensi dark mode ke localStorage
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem(
